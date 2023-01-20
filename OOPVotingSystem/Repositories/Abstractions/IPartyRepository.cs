0cbe403e61e16f98fa3448b64ff885f82b1b3be1
Moe <mbaksh@redev.co.uk>
2023-01-20 15:50:23 +0000

    Added template party code

---
+++ OOPVotingSystem/Repositories/Abstractions/IPartyRepository.cs
@@ -0,0 +1,22 @@
+﻿using OOPVotingSystem.Models;
+
+namespace OOPVotingSystem.Repositories.Abstractions;
+
+public interface IPartyRepository : IRepository<Party>
+{
+    void CreateCampaign(string id, string electionId);
+    
+    Task CreateCampaignAsync(string id, string electionId);
+
+    void CreateBudget(string id, string electionId);
+    
+    Task CreateBudgetAsync(string id, string electionId);
+
+    void CreateCost(string id, string electionId);
+    
+    Task CreateCostAsync(string id, string electionId);
+
+    double GetRemainingBudget(string id, string electionId);
+    
+    Task<double> GetRemainingBudgetAsync(string id, string electionId);
+}
---
+++ OOPVotingSystem/Repositories/PartyRepository.cs
@@ -0,0 +1,33 @@
+﻿using OOPVotingSystem.Models;
+using OOPVotingSystem.Repositories.Abstractions;
+
+namespace OOPVotingSystem.Repositories;
+
+public class PartyRepository : IPartyRepository
+{
+    public Task<Party> CreateAsync(Party entity) => throw new NotImplementedException();
+    
+    public void CreateBudget(string id, string electionId) => throw new NotImplementedException();
+    
+    public Task CreateBudgetAsync(string id, string electionId) => throw new NotImplementedException();
+    
+    public void CreateCampaign(string id, string electionId) => throw new NotImplementedException();
+    
+    public Task CreateCampaignAsync(string id, string electionId) => throw new NotImplementedException();
+    
+    public void CreateCost(string id, string electionId) => throw new NotImplementedException();
+    
+    public Task CreateCostAsync(string id, string electionId) => throw new NotImplementedException();
+    
+    public Task DeleteAsync(string id) => throw new NotImplementedException();
+    
+    public Task<IEnumerable<Party>> GetAllAsync() => throw new NotImplementedException();
+    
+    public Task<Party> GetByIdAsync(string id) => throw new NotImplementedException();
+    
+    public double GetRemainingBudget(string id, string electionId) => throw new NotImplementedException();
+    
+    public Task<double> GetRemainingBudgetAsync(string id, string electionId) => throw new NotImplementedException();
+    
+    public Task UpdateAsync(string id, Party entity) => throw new NotImplementedException();
+}
